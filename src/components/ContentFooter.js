57a4ed6f8e58b1e7b07f62e2b824fb5b4b9ddd2a
furkan-cloud <firstinfirstout2020@gmail.com>
2021-11-03 17:33:37 +0300

    Content Footer features for filter activated

--- src/components/ContentFooter.js
+++ src/components/ContentFooter.js
@@ -1,28 +1,56 @@
 import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { changeActiveFilter, clearCompleted } from "../redux/todos/todosSlice";
 
 const ContentFooter = () => {
+  const dispatch = useDispatch();
+  const items = useSelector((state) => state.todos.items);
+  const activeFilter = useSelector((state) => state.todos.activeFilter);
+  const itemsLeft = items.filter((item) => !item.completed).length;
+  console.log(itemsLeft);
   return (
     <footer className="footer">
       <span className="todo-count">
-        <strong>2</strong>
-        items left
+        <strong>{itemsLeft} </strong>
+        item{itemsLeft > 1 && "s"} left
       </span>
 
       <ul className="filters">
         <li>
-          <a href="/#" className="selected">
+          <a
+            href="/#"
+            onClick={() => dispatch(changeActiveFilter("all"))}
+            className={activeFilter === "all" ? "selected" : ""}
+          >
             All
           </a>
         </li>
         <li>
-          <a href="/#">Active</a>
+          <a
+            href="/#"
+            onClick={() => dispatch(changeActiveFilter("active"))}
+            className={activeFilter === "active" ? "selected" : ""}
+          >
+            Active
+          </a>
         </li>
         <li>
-          <a href="/#">Completed</a>
+          <a
+            href="/#"
+            onClick={() => dispatch(changeActiveFilter("completed"))}
+            className={activeFilter === "completed" ? "selected" : ""}
+          >
+            Completed
+          </a>
         </li>
       </ul>
 
-      <button className="clear-completed">Clear completed</button>
+      <button
+        className="clear-completed"
+        onClick={() => dispatch(clearCompleted())}
+      >
+        Clear completed
+      </button>
     </footer>
   );
 };
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -7,15 +7,27 @@ const TodoList = () => {
   const items = useSelector((state) => state.todos.items);
   console.log(items);
 
+  let filtered = [];
+
+  const activeFilter = useSelector((state) => state.todos.activeFilter);
+
   const handleDestroy = (id) => {
     if (window.confirm("Are you sure?")) {
       dispatch(destroy(id));
     }
   };
+  filtered = items;
+  if (activeFilter !== "all") {
+    filtered = items.filter((todo) =>
+      activeFilter === "active"
+        ? todo.completed === false && todo
+        : todo.completed === true && todo
+    );
+  }
 
   return (
     <ul className="todo-list">
-      {items.map((item) => (
+      {filtered.map((item) => (
         <li key={item.id} className={item.completed ? "completed" : ""}>
           <div className="view">
             <input
